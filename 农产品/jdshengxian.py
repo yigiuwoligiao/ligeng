9ea841d6d2140defbf911d3834284f898e0601d3
Administrator <648861325@qq.com>
2020-07-06 16:17:02 +0800

    剩余翻页不行,存储每次只能存一条,爬取第一页数据基本不会遇到阻碍了

--- 农产品/jdshengxian.py
+++ 农产品/jdshengxian.py
@@ -7,10 +7,13 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver import ChromeOptions
 import random
 import pymysql
+from selenium.webdriver.chrome.options import Options
 
 
 def main():
-    browser = webdriver.Chrome()
+    chrome_options = Options()
+    chrome_options.add_argument('--headless')
+    browser = webdriver.Chrome(options=chrome_options)
     browser.maximize_window()
     opt = webdriver.ChromeOptions()
     actions = ActionChains(browser)
@@ -35,105 +38,101 @@ def main():
     shop_list = wait.until(
         EC.presence_of_all_elements_located((By.XPATH, '//dd[contains(@class,"cate_con")]//a'))
     )
-    # for i in shop_list:
-    #     shop_list_url.append(i.get_attribute('href'))
+
     for i in shop_list:
-        i=i.get_attribute('href')
-        if len(i)>70:
+        i = i.get_attribute('href')
+        if len(i) > 70:
             shop_list_url.append(i)
-
-
+    test_ll = ()
     # 根据首页分类url,进一步解析
     for j in shop_list_url:
-        browser.get(j)
-        # 向下滚动到底部
-        browser.execute_script('window.scrollTo(0,document.body.scrollHeight-500)')
-        # 在下一页按钮旁边获取总页数
-        # next_btn=wait.until(
-        #     EC.presence_of_all_elements_located((By.XPATH,'//span[@class="p-skip"]/em/b'))
-        # )
-        # next_btn=int(next_btn[0].text())
-        # print(next_btn)
-        variety = browser.find_elements_by_class_name('search-key')[0]
-        variety=str(variety.text.strip())
-        print('当前品种-------', variety)
-        ls = wait.until(
-            EC.presence_of_all_elements_located((By.XPATH, '//div[@id="J_goodsList"]/ul/li[@class="gl-item"]'))
-        )
+        if len(j) > 0:
+            browser.get(j)
+            # 向下滚动到底部
+            browser.execute_script('window.scrollTo(0,document.body.scrollHeight-500)')
 
-        time.sleep(1)
-        for item in ls:
-            title = item.find_elements_by_xpath('.//div[@class="p-name p-name-type-2"]/a/em')[0]
-            title = str(title.text.strip())
-            print('标题:', title)
-            detail_url = item.find_elements_by_xpath('.//div[@class="p-name p-name-type-2"]/a')[0]
-            detail_url = detail_url.get_attribute('href')
-            print('商品链接:', detail_url)
-            price = item.find_elements_by_xpath('.//div[@class="p-price"]/strong/i')[0]
-            price = ''.join(price.text).strip()
-            print('价格:', price)
-            # 取得是评价数,趋于真实销量
-            xiaoliang = item.find_elements_by_xpath('.//div[@class="p-commit"]/strong/a')[0]
-            xiaoliang = xiaoliang.text.strip()
-            print('销量:', xiaoliang)
-            store = item.find_elements_by_xpath('.//div[@class="p-shop"]/span/a')
-            if len(store) >0:
-                print('店铺:', store[0].text.strip())
+            variety = browser.find_elements_by_class_name('search-key')[0]
+            variety = str(variety.text.strip().split('"')[1])
+            print('当前品种-------', variety)
+
+            # ls = wait.until(
+            #     EC.presence_of_all_elements_located((By.XPATH, '//div[@id="J_goodsList"]/ul/li[@class="gl-item"]'))
+            # )
+            ls=browser.find_elements_by_xpath('//div[@id="J_goodsList"]/ul/li[@class="gl-item"]')
+            time.sleep(1)
+            if len(ls)>0:
+                for item in ls:
+                    title = item.find_elements_by_xpath('.//div[@class="p-name p-name-type-2"]/a/em')[0]
+                    title = str(title.text.strip())
+                    print('标题:', title)
+                    detail_url = item.find_elements_by_xpath('.//div[@class="p-name p-name-type-2"]/a')[0]
+                    detail_url = detail_url.get_attribute('href')
+                    print('商品链接:', detail_url)
+                    price = item.find_elements_by_xpath('.//div[@class="p-price"]/strong/i')[0]
+                    price = ''.join(price.text).strip()
+                    print('价格:', price)
+                    # 取得是评价数,趋于真实销量
+                    xiaoliang = item.find_elements_by_xpath('.//div[@class="p-commit"]/strong/a')[0]
+                    xiaoliang = xiaoliang.text.strip()
+                    print('销量:', xiaoliang)
+                    store = item.find_elements_by_xpath('.//div[@class="p-shop"]/span/a')
+                    if len(store) > 0:
+                        store = store[0].text.strip()
+                        print('店铺:', store)
+                    else:
+                        store = '空'
+                        print('店铺:', store)
+                    img = item.find_elements_by_xpath('.//div[@class="p-img"]/a/img')[0]
+                    img = img.get_attribute('src')
+                    print('图片链接:', img)
+                    print('=' * 200)
+                    test_ll = (variety, title, detail_url, price, xiaoliang, store, img)
+                    # test_ls.append(variety)
+                    # test_ls.append(title)
+                    # test_ls.append(detail_url)
+                    # test_ls.append(price)
+                    # test_ls.append(xiaoliang)
+                    # test_ls.append(store)
+                    # test_ls.append(img)
             else:
-                store='空'
-                print('店铺:', store)
-            img = item.find_elements_by_xpath('.//div[@class="p-img"]/a/img')[0]
-            img = img.get_attribute('src')
-            print('图片链接:', img)
-            print('=' * 200)
-        # 翻页处理
-        browser.execute_script('window.scrollTo(0,document.body.scrollHeight-500);')
-        next_page_btn = wait.until(
-            EC.presence_of_element_located((By.CLASS_NAME, 'pn-next'))
-        )
-        print('next page...')
-        action = ActionChains(browser)
-        action.move_to_element(next_page_btn).click().perform()
-        time.sleep(random.random() * 2)
+                pass
+        else:
+            pass
+    return test_ll
 
-        conn=pymysql.connect(host='127.0.0.1',port=3306,db='ligeng',user='root',password='123456',charset='utf8')
-        cursor=conn.cursor()
-        sql='insert into jdshengxian(variety,title,detail_url,price,seles,store,img_url) values(%s,%s,%s,%s,%s,%s,%s)'
+    '''
+    翻页有问题
+    '''
+    # 翻页处理
+    browser.execute_script('window.scrollTo(0,document.body.scrollHeight-500);')
+    next_page_btn = wait.until(
+        EC.presence_of_element_located((By.CLASS_NAME, 'pn-next'))
+    )
+    print('next page...')
+    action = ActionChains(browser)
+    action.move_to_element(next_page_btn).click().perform()
+    time.sleep(random.random() * 2)
+    page_now = browser.find_element_by_xpath('//span[@class="p-skip"]/input[1]').get_attribute('value')
+    print('当前爬完了', page_now)
 
-        cursor.execute(sql,(variety,title,detail_url,price,xiaoliang,store,img))
+
+def write(test_ll):
+    try:
+        conn = pymysql.connect(host='127.0.0.1', port=3306, db='ligeng', user='root', password='123456',
+                               charset='utf8')
+        cursor = conn.cursor()
+        sql = 'insert into jdshengxian(variety,title,detail_url,price,seles,store,img_url) values(%s,%s,%s,%s,%s,%s,%s)'
+        args = (test_ll)
+        cursor.execute(sql, args=args)
         conn.commit()
         print('数据库保存成功')
-
-
-# def save_mysql('需要添加的数据名称'):
-#
-# 　　# 设置mysql连接
-# 　　connect = pymysql.connect(
-# 　　　　# IP为本地
-# 　　　　host='localhost'
-# 　　　　# 数据库名
-# 　　　　db='自己的数据库名'
-# 　　　　# 主机名
-# 　　　　user='root'
-# 　　　　# 密码，没有则用空字符串表示
-# 　　　　password='')
-# 　　# 创建游标
-# 　　cursor = connect.cursor()
-# 　　# 编写数据库语
-# 　　sql = "insert into yao('需要添加的数据名称') VALUES ('有多少个值就填写多少个'%s))"
-# 　　#例如：
-# 　　　　sql = "insert into yao(title,price,store) VALUES(%s,%s,%s)"
-# 　　# 判断插入是否成功
-# 　　try:
-# 　　　　curse.execute(sql,('需要添加的数据名称'))
-# 　　　　#例如：
-# 　　　　　　curse.execute(sql,(title,price,store))
-# 　　　　connect.commit()
-# 　　　　print('数据插入成功')
-# 　　except:
-# 　　　　print('数据插入失败')
-
+    except Exception as e:
+        print(e)
 
 
 if __name__ == '__main__':
-    main()
+    start=time.time()
+    test_ls = main()
+    write(test_ls)
+    end=time.time()
+    print('用时',end-start)
